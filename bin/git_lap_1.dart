38c285957610a990fdf3371812136cac1f4ee629
FaHaDoF69 <93477293+FaHaDoF69@users.noreply.github.com>
2024-07-31 11:42:37 +0300

    delete some file and update main function

--- bin/git_lap_1.dart
+++ bin/git_lap_1.dart
@@ -1,5 +1,5 @@
-import 'package:git_lap_1/git_lap_1.dart' as git_lap_1;
 
-void main(List<String> arguments) {
-  print('Hello world: ${git_lap_1.calculate()}!');
+// Here we go
+void main() {
+  print('Hello world');
 }
--- test/git_lap_1_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:git_lap_1/git_lap_1.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
